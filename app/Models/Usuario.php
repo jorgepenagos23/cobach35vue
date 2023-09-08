0161f8952985e49ec1bed25223f0d8075483ad70
jorge_ramirez_penagos <jorgepenagos50@gmail.com>
2023-09-08 03:50:08 -0600

    07 septiembre 2023 excel alumnos mysql laravel main A

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -7,12 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-<<<<<<< HEAD:app/Models/usuario_has_roles.php
 use Illuminate\Notifications\Notifiable;
 class usuarios_has_roles extends Authenticatable
-=======
-class Usuario extends Model
->>>>>>> copia_main:app/Models/Usuario.php
 {
     use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'usuario_has_roles';
