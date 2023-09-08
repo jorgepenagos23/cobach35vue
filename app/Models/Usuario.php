f948e13c4d5f369c0dff6e535e0d0bc12820865a
jorge_ramirez_penagos <jorgepenagos50@gmail.com>
2023-09-08 03:49:00 -0600

    07 septiembre 2023 excel alumnos mysql laravel

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
 class Usuario extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'usuario_has_roles';
+
+    protected $fillable = [
+
+        'id',
+        'usuario_id',
+        'matricula',
+        'rol_id'
+
+
+    ];
+
+    //agregar  la relacion de usuario con la publicacion
+
+    public function Publicaciones()
+{
+    return $this->hasMany(Publicaciones::class);
+}
+
+
+
+
+
 }
